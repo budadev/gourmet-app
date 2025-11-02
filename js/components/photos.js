590232ed693d7dce3c10f2e778c335ca12cd2c71
Roland Budai <budai.roland.89@gmail.com>
2025-11-02 13:37:25 +0400

    Update thumbnail dimensions and bump version to 0.1.59

--- js/components/photos.js
+++ js/components/photos.js
@@ -21,7 +21,7 @@ export function blobToDataURL(blob) {
   });
 }
 
-async function createThumbnailBlob(dataURL, maxW=150, maxH=150) {
+async function createThumbnailBlob(dataURL, maxW=600, maxH=600) {
   // Create an Image from the dataURL
   const img = await new Promise((resolve, reject) => {
     const i = new Image();
